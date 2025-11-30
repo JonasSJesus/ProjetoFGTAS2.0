38d743a3f0b23767e4c97fb3b1a255ac0a3488f6
Jonas Silva <jonas.silva502@gmail.com>
2025-11-30 20:01:01 -0300

    tests: adiciona estrutura de testes com phpunit

---
+++ tests/Controller/ExampleControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleControllerTest extends TestCase
+{
+    public function test_true_is_true()
+    {
+        self::assertTrue(true);
+    }
+}
